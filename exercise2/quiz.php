74666ca837b368b77af92471f338f648ddeff0ae
Vellwulf <89924600+Vellwulf@users.noreply.github.com>
2021-11-30 12:05:13 -0600

    Add third quiz question

--- exercise2/quiz.php
+++ exercise2/quiz.php
@@ -6,7 +6,7 @@
 </style>
 <body>
 <?php 
-	$userAnswers = array($_POST["q1Ans"], $_POST["q2Ans"]);
+	$userAnswers = array($_POST["q1Ans"], $_POST["q2Ans"], $_POST["q3Ans"]);
 ?>
 
 	Question 1: Armadillos keep ___<br>
@@ -15,6 +15,10 @@
 	
 	Question 2: How are you feeling?<br>
 	<p class="answer">Your answer: <?php echo $userAnswers[1]; ?></p>
-	<p class="answer">Correct answer: Do you like the show?</p>
+	<p class="answer">Correct answer: Do you like the show?</p><br>
+	
+	Question 3: Who's the president?<br>
+	<p class="answer">Your answer: <?php echo $userAnswers[2]; ?></p>
+	<p class="answer">Correct answer: Byron</p><br>
 </body>
 </html>
