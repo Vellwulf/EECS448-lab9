d56235666f6f4c05a97184f6002658c0306f5599
Vellwulf <89924600+Vellwulf@users.noreply.github.com>
2021-11-29 21:55:30 -0600

    Add 100x100 multiplication table

--- exercise1.php
+++ exercise1.php
@@ -1,3 +1,25 @@
 <?php
-  echo "<h1>Multiplication Table</h1>";
+	function makeRow($row) {
+		echo "<th>$row</th>";
+		
+		for ($col = 1; $col <= 100; $col++) {
+			echo "<td>" . $row * $col . "</td>";
+		}
+	}
+
+	echo "<h1>Multiplication Table</h1>";
+  
+	echo "<table><tr>";
+	echo "<td> </td>";
+  
+	for ($x = 1; $x <= 100; $x++)
+		echo "<th>$x</th>";
+  
+	echo "</tr>";
+  
+	for ($x = 1; $x <= 100; $x++) {
+		echo "<tr>";
+		makeRow($x);
+		echo "</tr>";
+	}
 ?>
