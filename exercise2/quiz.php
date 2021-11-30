137d9f9db80a6a3d05fe7d47b971fa941be24af7
Vellwulf <89924600+Vellwulf@users.noreply.github.com>
2021-11-30 12:09:42 -0600

    Add answer key array

--- exercise2/quiz.php
+++ exercise2/quiz.php
@@ -7,18 +7,19 @@
 <body>
 <?php 
 	$userAnswers = array($_POST["q1Ans"], $_POST["q2Ans"], $_POST["q3Ans"]);
+	$ansKey = array("digging holes in my backyard", "Do you like the show?", "Byron");
 ?>
 
 	Question 1: Armadillos keep ___<br>
 	<p class="answer">Your answer: <?php echo $userAnswers[0]; ?></p>
-	<p class="answer">Correct answer: digging holes in my backyard</p><br>
+	<p class="answer">Correct answer: <?php echo $ansKey[0]; ?></p><br>
 	
 	Question 2: How are you feeling?<br>
 	<p class="answer">Your answer: <?php echo $userAnswers[1]; ?></p>
-	<p class="answer">Correct answer: Do you like the show?</p><br>
+	<p class="answer">Correct answer: <?php echo $ansKey[1]; ?></p><br>
 	
 	Question 3: Who's the president?<br>
 	<p class="answer">Your answer: <?php echo $userAnswers[2]; ?></p>
-	<p class="answer">Correct answer: Byron</p><br>
+	<p class="answer">Correct answer: <?php echo $ansKey[2]; ?></p><br>
 </body>
 </html>
