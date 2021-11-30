d62be8fca4ef055af167d447f532103326894bba
Vellwulf <89924600+Vellwulf@users.noreply.github.com>
2021-11-30 11:26:30 -0600

    Create quiz grader php

---
+++ exercise2/quiz.php
@@ -0,0 +1,14 @@
+<html>
+<style>
+	p.answer {
+		text-indent: 35px;
+	}
+</style>
+<body>
+	Question 1: Armadillos keep ___<br>
+	<p class="answer">Your answer: <?php echo $_POST["q1Ans"]; ?></p>
+	<p class="answer">Correct answer: digging holes in my backyard</p><br>
+	
+	Question 2:
+</body>
+</html>
