c7561d488ed077d165569e5ffd5f507a87db4165
Vellwulf <89924600+Vellwulf@users.noreply.github.com>
2021-11-30 11:53:43 -0600

    Add array of user answers

--- exercise2/quiz.php
+++ exercise2/quiz.php
@@ -5,12 +5,16 @@
 	}
 </style>
 <body>
+<?php 
+	$userAnswers = array($_POST["q1Ans"], $_POST["q2Ans"]);
+?>
+
 	Question 1: Armadillos keep ___<br>
-	<p class="answer">Your answer: <?php echo $_POST["q1Ans"]; ?></p>
+	<p class="answer">Your answer: <?php echo $userAnswers[0]; ?></p>
 	<p class="answer">Correct answer: digging holes in my backyard</p><br>
 	
 	Question 2: How are you feeling?<br>
-	<p class="answer">Your answer: <?php echo $_POST["q2Ans"]; ?></p>
+	<p class="answer">Your answer: <?php echo $userAnswers[1]; ?></p>
 	<p class="answer">Correct answer: Do you like the show?</p>
 </body>
 </html>
