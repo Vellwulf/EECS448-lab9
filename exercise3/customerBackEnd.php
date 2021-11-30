fbf9229d2fb945b8fda989cf1f3f855c617f6f3f
Vellwulf <89924600+Vellwulf@users.noreply.github.com>
2021-11-30 16:08:20 -0600

    Create customer back end file

---
+++ exercise3/customerBackEnd.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+	<?php
+		$jellyNum = $_POST["jelly-legs"];
+		$tripNum = $_POST["trip"];
+		$pepperNum = $_POST["pepper-breath"];
+		$shipMethod = $_POST["shipSelect"];
+		
+		echo "jelly: " . $jellyNum;
+	?>
+</body>
+</html>
