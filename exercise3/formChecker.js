c00e1aeafc2365db945a7a2e17c4e1cf665fddae
Vellwulf <89924600+Vellwulf@users.noreply.github.com>
2021-11-30 18:57:55 -0600

    Update item quantity error message to identify culprit

--- exercise3/formChecker.js
+++ exercise3/formChecker.js
@@ -1,11 +1,11 @@
-function checkQuantity (inputValue) {
+function checkQuantity (inputValue, inputField) {
 	if (inputValue == "") {
-		alert("Error: a quantity must be entered for each store item (greater than or equal to 0).");
+		alert("Error: a quantity must be entered for the " + inputField + " amount (greater than or equal to 0).");
 		return false;
 	}
 	
 	else if (inputValue < 0) {
-		alert("Error: store item quantities cannot be negative. Please enter 0 or a positive number.");
+		alert("Error: the " + inputField + " amount cannot be negative. Please enter 0 or a positive number.");
 		return false;
 	}
 	
@@ -20,17 +20,24 @@ function checkShipSelection (selection) {
 	return true;
 }
 
+function checkUsername(username) {
+	if (username == "") {
+		alert("Error: an email address must be entered in the username field (ex: name@domain.com)");
+		return false;
+	}
+}
+
 function validate() {
 	let isValid = true;
 	let form = document.forms["storeForm"];
 	
-	if (!checkQuantity(form["jelly-legs"].value))
+	if (!checkQuantity(form["jelly-legs"].value, "Jelly-Legs Jinx"))
 		isValid = false;
 	
-	if (!checkQuantity(form["trip"].value))
+	if (!checkQuantity(form["trip"].value, "Trip Jinx"))
 		isValid = false;
 	
-	if (!checkQuantity(form["pepper-breath"].value))
+	if (!checkQuantity(form["pepper-breath"].value, "Pepper Breath Hex"))
 		isValid = false;
 	
 	if (!checkShipSelection(form["shipSelect"].value))
