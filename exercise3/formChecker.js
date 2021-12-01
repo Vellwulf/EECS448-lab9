dc44a8110ff98b16fb4c5f5a5dd2fcf20716758c
Vellwulf <89924600+Vellwulf@users.noreply.github.com>
2021-11-30 18:25:00 -0600

    Feat check item quantities function

--- exercise3/formChecker.js
+++ exercise3/formChecker.js
@@ -1,4 +1,21 @@
-function validate() {
-	console.log("got here");
+function checkQuantity (inputValue) {
+	if (inputValue == "") {
+		alert("Error: a quantity must be entered for each store item (greater than or equal to 0).");
+		return false;
+	}
+	
+	else if (inputValue < 0) {
+		alert("Error: store item quantities cannot be negative. Please enter 0 or a positive number.");
+		return false;
+	}
+	
 	return true;
+}
+
+function validate() {
+	let isValid = true;
+	
+	isValid = checkQuantity(document.forms["storeForm"]["jelly-legs"].value);
+	alert("isValid = " + isValid);
+	return isValid;
 }
